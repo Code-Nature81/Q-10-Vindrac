fc3799bacb212bb366953d212f6aa19a90194b4b
Code-Nature81 <81328065+Code-Nature81@users.noreply.github.com>
2024-05-04 17:09:28 +0200

    Add files via upload

---
+++ question.js
@@ -0,0 +1,29 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 10,
+    question: "Jusqu’en quelle année selon vous ?",
+    answer: "4) 1980",
+    options: [
+      "1) 1950",
+      "2) 1960",
+      "3) 1970",
+      "4) 1980"
+    ]
+  },
+   
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
